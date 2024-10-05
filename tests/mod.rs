acdf6d4288baf57e235d389a4de5193e29d09117
Eekle <96976531+Eekle@users.noreply.github.com>
2024-10-05 16:08:46 +0200

    Remove Anyhow. More Tests.

--- tests/mod.rs
+++ tests/mod.rs
@@ -15,12 +15,13 @@ impl<'a> maschen::Token for StringToken<'a> {
             "," => TokenKind::FnSeparator,
             "sin" | "cos" | "tan" | "log" => TokenKind::Function(1),
             "max" | "min" => TokenKind::Function(2),
-            _ => TokenKind::Value,
+            x if isize::from_str(x).is_ok() => TokenKind::Value,
+            x => panic!("{} is not an int", x),
         }
     }
 }
-fn test_in_out(inp: &str, outp: &str) -> Result<(), maschen::Error> {
-    let stream = inp.split(' ').map(StringToken);
+fn test_run(inp: &str) -> Result<String, maschen::Error> {
+    let stream = inp.split_whitespace().map(StringToken);
     let mut yard = ShuntingYard::default();
     for v in stream {
         yard.process(v)?;
@@ -31,65 +32,68 @@ fn test_in_out(inp: &str, outp: &str) -> Result<(), maschen::Error> {
         yard_output.push(' ');
         yard_output.push_str(tok.0);
     }
-    assert_eq!(outp, yard_output.as_str());
-    Ok(())
-}
-#[test]
-fn simple_value() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("3", "3")
+    Ok(yard_output)
 }
 
-#[test]
-fn single_infix_op() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("3 + 3", "3 3 +")
+macro_rules! test_works {
+    ($name: ident, $in: expr, $out: expr) => {
+        #[test]
+        fn $name() -> Result<(), maschen::Error> {
+            assert_eq!(test_run($in)?.as_str(), $out);
+            Ok(())
+        }
+    };
 }
 
-#[test]
-fn single_unary_op() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("~ 3", "3 ~")
+macro_rules! test_fails {
+    ($name: ident, $in: expr, $patt: pat) => {
+        #[test]
+        fn $name() {
+            assert!(matches!(test_run($in), Err($patt)))
+        }
+    };
 }
 
-#[test]
-fn func() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("log ( 3 )", "3 log")
-}
+test_works!(simple_value, "3", "3");
+test_works!(single_infix_op, "3 + 3", "3 3 +");
+test_works!(single_unary_op, "~ 3", "3 ~");
+test_works!(func, "log ( 3 )", "3 log");
+test_works!(precendence, "3 + 4 * 5", "3 4 5 * +");
+test_works!(left_assoc, "3 + 4 - 5", "3 4 + 5 -");
+test_works!(unary_and_infix, "3 * ~ 4", "3 4 ~ *");
+test_works!(
+    unary_and_infix_and_func,
+    "3 * log ( ~ 4 + 5 )",
+    "3 4 ~ 5 + log *"
+);
+test_works!(nested_braces, "3 * ( 4 + ( 2 - 1 ) )", "3 4 2 1 - + *");
+test_works!(fn2, "max ( 2 , 3 )", "2 3 max");
+test_works!(fn_with_nested, "max ( ( ( 2 , 3 ) ) )", "2 3 max");
+test_works!(
+    nested_fns,
+    "max ( 3 , max ( log ( 4 ) , 2 ) )",
+    "3 4 log 2 max max"
+);
 
-#[test]
-fn precendence() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("3 + 4 * 5", "3 4 5 * +")
-}
-
-#[test]
-fn left_assoc() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("3 + 4 - 5", "3 4 + 5 -")
-}
-
-#[test]
-fn unary_and_infix() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("3 * ~ 4", "3 4 ~ *")
-}
-
-#[test]
-fn unary_and_infix_and_func() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("3 * log ( ~ 4 + 5 )", "3 4 ~ 5 + log *")
-}
-
-#[test]
-fn nested_braces() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("3 * ( 4 + ( 2 - 1 ) )", "3 4 2 1 - + *")
-}
-
-#[test]
-fn fn2() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("max ( 2 , 3 )", "2 3 max")
-}
-
-#[test]
-fn fn_with_nested() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("max ( ( ( 2 , 3 ) ) )", "2 3 max")
-}
-
-#[test]
-fn nested_fns() -> anyhow::Result<(), maschen::Error> {
-    test_in_out("max ( 3 , max ( log ( 4 ) , 2 ) )", "3 4 log 2 max max")
-}
+test_fails!(empty, "", maschen::Error::Malformed);
+test_fails!(bad_infix1, "3 3 +", maschen::Error::Malformed);
+test_fails!(bad_infix2, "+ 3 3", maschen::Error::Malformed);
+test_fails!(arg_count1, "min", maschen::Error::FunctionLen);
+test_fails!(arg_count2, "min ( 2 )", maschen::Error::FunctionLen);
+test_fails!(arg_count3, "log ( 2 , 3 )", maschen::Error::FunctionLen);
+test_fails!(empty_parens, "( )", maschen::Error::Malformed);
+test_fails!(
+    unbalanced_parens1,
+    "log ( 2",
+    maschen::Error::UnbalancedParens
+);
+test_fails!(
+    unbalanced_parens2,
+    "log ( 2",
+    maschen::Error::UnbalancedParens
+);
+test_fails!(
+    unbalanced_parens3,
+    "log ( 2 ) )",
+    maschen::Error::UnbalancedParens
+);
